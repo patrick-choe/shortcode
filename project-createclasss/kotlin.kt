e1ce8997bf4c994563731553cfd7dec933709ace
Patrick <jehyeokchoe@gmail.com>
2020-10-08 20:47:16 +0900

    Update kotlin.kt (PatrickKR)

--- project-createclasss/kotlin.kt
+++ project-createclasss/kotlin.kt
@@ -1,2 +1,5 @@
 // 59 parangee
-class waClass(var a:String){fun hi(){println("${a}, 샌즈.")}}
+//class waClass(var a:String){fun hi(){println("${a}, 샌즈.")}}
+
+// 54 PatrickKR
+class waClass(var s:String){fun hi()=print("$s, 샌즈.")}
